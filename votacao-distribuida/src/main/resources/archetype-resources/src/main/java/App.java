eb57f0ad62abbf5c58dcbc8d0781f5c10037061c
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-07-16 10:25:14 -0300

    feat: base project

---
+++ votacao-distribuida/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.voting-system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
